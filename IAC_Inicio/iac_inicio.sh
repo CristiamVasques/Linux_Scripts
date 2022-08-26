f89f808f601240604c5cfce0d35720fb8eda949f
CristiamVasques <crvasques@outlook.com>
2022-08-26 02:26:11 -0300

    inicio

--- IAC_Inicio/iac_inicio.sh
+++ IAC_Inicio/iac_inicio.sh
@@ -17,17 +17,17 @@ groupadd GRP_SEC
 
 echo "Criando usuários e vínculo a grupos"
 
-useradd carlos -m -c "Carlos Finney" -s /bin/bash -p $(openssl passwd -crypt Projeto@01) -G GRP_ADM
-useradd maria -m -c "Maria Madalena" -s /bin/bash -p $(openssl passwd -crypt Projeto@01) -G GRP_ADM
-useradd joao -m -c "João Maria Hermel" -s /bin/bash -p $(openssl passwd -crypt Projeto@01) -G GRP_ADM
+useradd carlos -m -c "Carlos Finney" -s /bin/bash -p $(openssl passwd -crypt Proj@001) -G GRP_ADM
+useradd maria -m -c "Maria Madalena" -s /bin/bash -p $(openssl passwd -crypt Proj@001) -G GRP_ADM
+useradd joao -m -c "João Maria Hermel" -s /bin/bash -p $(openssl passwd -crypt Proj@001) -G GRP_ADM
 
-useradd debora -m -c "Debora Lapidote" -s /bin/bash -p $(openssl passwd -crypt Projeto@01) -G GRP_VEN
-useradd sebastiana -m -c "Sebastiana Graça de Jesus" -s /bin/bash -p $(openssl passwd -crypt Projeto@01) -G GRP_VEN
-useradd roberto -m -c "Roberto Evan" -s /bin/bash -p $(openssl passwd -crypt Projeto@01) -G GRP_VEN
+useradd debora -m -c "Debora Lapidote" -s /bin/bash -p $(openssl passwd -crypt Proj@001) -G GRP_VEN
+useradd sebastiana -m -c "Sebastiana Graça de Jesus" -s /bin/bash -p $(openssl passwd -crypt Proj@001) -G GRP_VEN
+useradd roberto -m -c "Roberto Evan" -s /bin/bash -p $(openssl passwd -crypt Proj@001) -G GRP_VEN
 
-useradd josefina -m -c "Josefina Pará" -s /bin/bash -p $(openssl passwd -crypt Projeto@01) -G GRP_SEC
-useradd amanda -m -c "Amanda da Graça" -s /bin/bash -p $(openssl passwd -crypt Projeto@01) -G GRP_SEC
-useradd rogerio -m -c "Rogério Evangelista" -s /bin/bash -p $(openssl passwd -crypt Projeto@01) -G GRP_SEC
+useradd josefina -m -c "Josefina Pará" -s /bin/bash -p $(openssl passwd -crypt Proj@001) -G GRP_SEC
+useradd amanda -m -c "Amanda da Graça" -s /bin/bash -p $(openssl passwd -crypt Proj@001) -G GRP_SEC
+useradd rogerio -m -c "Rogério Evangelista" -s /bin/bash -p $(openssl passwd -crypt Proj@001) -G GRP_SEC
 
 echo "Determinando que os usuários cadastrem nova senha"
 
@@ -47,9 +47,9 @@ chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
 
+chmod 777 /publico
 chmod 770 /adm
 chmod 770 /ven
 chmod 770 /sec
-chmod 777 /publico
 
 echo "Fine della missione"
